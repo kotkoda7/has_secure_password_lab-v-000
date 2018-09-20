6664def47b2e2ca4d07c59b006307f5d191786eb
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-20 20:53:05 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to root_path
     else
-      
+      flash[:message]= "incorrect login"
 end
   
 end

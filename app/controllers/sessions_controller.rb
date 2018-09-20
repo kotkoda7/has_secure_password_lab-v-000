53670214a0374ab8924ee86daabf70fabd1fe8a4
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-20 20:55:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
   
   def create
-    
-    @user = User.find(name: params[:user][:name]
+    @user = User.find_by(name: params[:user][:name])
     if @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to "/"
     else
       flash[:message]= "incorrect login"
       render :login
     end
-end
+  end
+
   
 end

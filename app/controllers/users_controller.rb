009ce178ef1f56950a5bc9b6c0ca674f32ab68d4
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-20 21:15:57 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save 
       session[:user_id] = @user.id
-      redirect_to root_url
+      redirect_to "/"
     else
       redirect_to new_user_path
   end

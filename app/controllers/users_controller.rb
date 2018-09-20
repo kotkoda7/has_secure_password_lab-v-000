74d4af0fd52d04cc1c598c921a79c82a4d8ff472
Szilvia Hosser-Cox <szilvia.hosser@gmail.com>
2018-09-20 19:50:23 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     
   def create
     @user = User.new
-    if session()
+    if @user.save 
+      session(:user_id) = user.id
     
     render :new
   end
